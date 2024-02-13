04149b308c9f5542603381a71b9d6109c3937f87
Pedro <pedrolins1309@gmail.com>
2024-02-13 09:55:04 -0300

    more refactoring, fixed datawidth output of weights and bias

--- mnist/src/zynet/utils.py
+++ mnist/src/zynet/utils.py
@@ -1,10 +1,41 @@
 import json
 import math
+# TODO remove bitstring dependency
+import bitstring
+
+'''
+    DtoB converts your floating point number into fixed point representation with fracBits bits dedicated
+    to the decimal part and (dataWidth - fracBits) dedicated to the integer part
+'''
+def floatToFix(floatNumber, fracBits, totalBits):             
+    fixPoint = int(round(abs(floatNumber) * (2**fracBits)))
+    fixPointBinary = bitstring.BitArray(int=fixPoint, length=totalBits)
+    bin_list = list(fixPointBinary.bin)
+   
+    if(floatNumber < 0):
+        bin_list[0] = "1"
+
+    return ''.join(bin_list)
+
+def fixedToFloat(bin_str, fracBits, totalBits) -> float:
+    
+    bit_list = list(bin_str)
+    sign = 1
+    if(bit_list[0] == "1"):
+        sign = -1
+        bit_list[0] = "0"
+
+    bin_str = ''.join(bit_list)
+
+    intRepresentation = int(bin_str, 2)
+
+    return sign * (intRepresentation * 1.0)/(2 ** fracBits)
 
 def genWeightArray(filename=""):
     with open(filename, "r") as f:
         output = []
         myWeights = json.load(f)['weights']
+        # TODO remove this flatten
         flatten(myWeights,output)
         #maxVal = max(output)
         #minVal = min(output)
@@ -13,12 +44,9 @@ def genWeightArray(filename=""):
         return myWeights
     
     
-def genBiasArray(fileName=""):
-	biasFile = open(fileName,"r")
-	myData = biasFile.read()
-	myDict = json.loads(myData)
-	myBiases = myDict['biases']
-	return myBiases
+def genBiasArray(filename=""):
+    with open(filename, "r") as f:
+        return json.loads(f.read())['biases']
     
     
 def flatten(l,output): #Recursive function to flatten a multi-dimensional list
